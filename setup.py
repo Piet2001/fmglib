1663c705ecd3584bb63ae76214186a629d92f03c
Piet2001 <54183168+Piet2001@users.noreply.github.com>
2025-01-07 16:05:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
       name='ftntlib',
       version='0.4.0.dev20',
       description='Python modules to interact with Fortinet products',
-      install_requires=['requests','suds-jurko','lxml'],
+      install_requires=['requests','suds-community','lxml'],
       author='Original: Ashton Turpin, Maintainer: Jean-Pierre Forcioli, Contributors: Jeremy Parente',
       author_email='jpforcioli@fortinet.com, jparente@fortinet.com',
       url='https://fndn.fortinet.net',
